275ed7ecdfd841b3f19e68f45ca8bc3e0431c992
9train <Rafael@9trainproductions.com>
2026-04-09 13:48:57 -0400

    Add shared ControllerState and FLX6 state integration

--- src/controllers/core/index.js
+++ src/controllers/core/index.js
@@ -1,11 +1,7 @@
 export * from './contracts.js';
 export * from './aliases.js';
-export * from './feel.js';
-export * from './hooks.js';
 export * from './normalization.js';
 export * from './state.js';
-export * from './truth.js';
-export * from './ui.js';
 export * from './vocabulary.js';
 
-export const controllerCoreReady = true;
+export const controllerCoreReady = false;
