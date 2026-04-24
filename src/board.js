c3ab65a15162e01b636b16adf9681186200d84bc
9train <Rafael@9trainproductions.com>
2026-04-23 20:39:36 -0400

    Delete src directory

--- src/board.js
+++
@@ -1,7 +0,0 @@
-export { decodeRelative7, getContinuousRenderValue, getLinearControlPosition, getLinearControlRatio, shouldHoldBinaryVisualState } from './board/controls.js';
-export { inspectBoardTarget } from './board/inspection.js';
-export { remergeLearned } from './board/map-store.js';
-export { resolveBoardSelectionFromElement, resolveCanonicalRenderTargetId, resolveRenderTargetId } from './board/profile.js';
-export { resolveInfoRenderPlan, resolveInfoRenderTarget } from './board/render-plan.js';
-export { getBoardSvgRoot, getUnifiedMap } from './board/state.js';
-export { consumeInfo, initBoard } from './board/runtime.js';
